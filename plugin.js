11d1f7353439a51d96a72e4ac355adf6ecf88db6
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-07-25 04:16:42 +0200

    Added animation support

--- plugin.js
+++ plugin.js
@@ -1,33 +1,45 @@
 (function(){
     var w = 16, h = 16;
     var img = document.getElementsByClassName('largeavatar')[0];
-    img.style.visibility = 'hidden';
+    var original_img = document.createElement('img');
     var loaded = false;
-    img.onload = function(){
+    function load(_, _loaded){
         if(loaded) return;
-        loaded = true;
+        loaded = !_loaded;
         var original = document.createElement('canvas');
-        original.width = w;
-        original.height = h;
         var octx = original.getContext('2d');
-        octx.drawImage(img,0,0);
-        var oImageData = octx.getImageData(0,0,w,h);
         var canvas = document.createElement('canvas');
-        canvas.width = img.width;
-        canvas.height = img.height;
         var ctx = canvas.getContext('2d');
+        canvas.height = img.height;
+        original.height = h;
+        img.parentNode.appendChild(canvas);
+        img.parentNode.removeChild(img);
+        canvas.width = img.width;
         var imageData = ctx.getImageData(0,0,canvas.width,canvas.height); 
-        var r = w/canvas.width;
-        for(var x=0;x<canvas.width;x++){
-            for(var y=0;y<canvas.height;y++){
-                imageData.data[(y*canvas.width + x) * 4 + 0] = oImageData.data[((y*r|0)*w + x*r|0) * 4 + 0];
-                imageData.data[(y*canvas.width + x) * 4 + 1] = oImageData.data[((y*r|0)*w + x*r|0) * 4 + 1];
-                imageData.data[(y*canvas.width + x) * 4 + 2] = oImageData.data[((y*r|0)*w + x*r|0) * 4 + 2];
-                imageData.data[(y*canvas.width + x) * 4 + 3] = 255;
+        (function(){
+            original.width = w;
+            octx.drawImage(original_img,0,0);
+            var oImageData = octx.getImageData(0,0,w,h);
+            canvas.width = img.width;
+            var r = w/canvas.width;
+            var iData = imageData.data;
+            var cw = canvas.width;
+            var ch = canvas.height;
+            var oData = oImageData.data;
+            for(var x=0;x<cw;x++){
+                for(var y=0;y<ch;y++){
+                    var ii = (y*cw+x)*4;
+                    var oi = ((y*r|0)*w + x*r|0) * 4;
+                    iData[ii    ] = oData[oi    ];
+                    iData[ii + 1] = oData[oi + 1];
+                    iData[ii + 2] = oData[oi + 2];
+                    iData[ii + 3] = oData[oi + 3];
+                }
             }
-        }
-        ctx.putImageData(imageData, 0, 0);
-        img.src = canvas.toDataURL();
-        img.style.visibility = 'visible';
+            ctx.putImageData(imageData, 0, 0);
+            setTimeout(arguments.callee, 30);
+        })();
     };
+    original_img.onload = load;
+    original_img.src = img.src;
 })();
